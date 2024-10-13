519576906001cfb08ba5a3caa0e294a6ad577bef
[JuliQ89] <[julian.singheiser@gmail.com]>
2024-10-13 18:29:58 +0200

    style Home Header

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import note_undraw from "../assets/images/note_undraw.svg";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="headerContainer">
+      <div className="headerColLeft">
+        <div className="headerTextContainer">
+          <h1>Quicknote your Notes!</h1>
+          <h3>Hier kannst du deine Notizen erstellen und verwalten!</h3>
+          <p>
+            Quicknote hilft dir, deine Gedanken zu organisieren und wichtige
+            Informationen schnell und einfach zu speichern. Ob berufliche
+            Aufgaben, persönliche Projekte oder spontane Ideen – mit Quicknote
+            hast du deine Notizen immer griffbereit. Behalte den Überblick und
+            nutze Quicknote, um jederzeit auf deine Inhalte zuzugreifen und sie
+            übersichtlich zu verwalten.
+          </p>
+        </div>
+      </div>
+      <div className="headerColRight">
+        <img src={note_undraw} alt="" />
+      </div>
     </div>
   );
 };
