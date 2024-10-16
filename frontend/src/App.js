c0d88aceb13ea131fbfcbf02268ff0f8813410d1
[JuliQ89] <[julian.singheiser@gmail.com]>
2024-10-16 17:01:19 +0200

    create .env file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import { loginUserOnPageLoad } from "./utils/auth";
 function App() {
   useEffect(() => {
     loginUserOnPageLoad();
+    console.log(process.env.REACT_APP_BACKEND_URL);
   }, []);
 
   return (
